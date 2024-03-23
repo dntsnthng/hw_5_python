aecb10df543a655282f95fe3bd716ba93d449b5c
Elmin Gulamov <g.elmn@yandex.ru>
2024-03-24 00:19:50 +0400

    add something

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,12 @@ import os
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
+    browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = os.getenv('window_width', '1600')
     browser.config.window_height = os.getenv('window_height', '1200')
-    browser.config.timeout = float(os.getenv('timeout', '3.0'))
     browser.config.hold_browser_open = True
+
+    yield
+    browser.quit()
+
+
---
+++ tests/test_someone.py
@@ -0,0 +1,54 @@
+import os.path
+import time
+
+import pytest
+from selene import browser, be, have, command
+
+
+def test_web():
+    browser.open('https://demoqa.com/automation-practice-form')
+    browser.element('#firstName').set_value("Dima")
+    browser.element('#lastName').set_value("Botkin")
+    browser.element('[for="gender-radio-1"]').click()
+    browser.element('#userNumber').set_value("8800555353")
+    browser.element('#userEmail').set_value("sobaka@mail.com")
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__month-select').click().element('[value="0"]').click()
+    browser.element('.react-datepicker__year-select').send_keys('1991')
+    browser.element('.react-datepicker__day--026').click()
+    browser.element('#subjectsInput').type('Math').press_enter()
+    browser.element('[for="hobbies-checkbox-1"]').click()
+    browser.element('#uploadPicture').send_keys(os.path.abspath('l3.jpg'))
+    browser.element('#currentAddress').type('usa')
+    browser.element('#react-select-3-input').type('NCR').press_enter()
+    browser.element('#react-select-4-input').type('delhi').press_enter()
+    browser.element('#submit').click()
+
+    browser.element('[class="modal-content"]').should(have.text('Dima Botkin'))
+    browser.element('[class="modal-content"]').should(have.text('sobaka@mail.com'))
+    browser.element('[class="modal-content"]').should(have.text('Male'))
+    browser.element('[class="modal-content"]').should(have.text('8800555353'))
+    browser.element('[class="modal-content"]').should(have.text('26 January,1991'))
+    browser.element('[class="modal-content"]').should(have.text('Math'))
+    browser.element('[class="modal-content"]').should(have.text('Sports'))
+    browser.element('[class="modal-content"]').should(have.text('l3.jpg'))
+    browser.element('[class="modal-content"]').should(have.text('usa'))
+    browser.element('[class="modal-content"]').should(have.text('NCR Delhi'))
+    browser.element('[class="modal-content"]').should(have.text('Thanks for submitting the form'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
